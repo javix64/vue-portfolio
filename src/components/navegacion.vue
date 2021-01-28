bad6eb6b6e7a0b412432980e39ca20a714fe744d
javix64 <jav1.x64@gmail.com>
2021-01-28 09:09:11 +0100

    first commit

---
+++ src/components/navegacion.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <section class="navegacion">
+    <h1> Esto es la navegacion</h1>
+  </section>
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'navegacion',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/portfolio.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+
+  <section class="portfolio">
+    <h1>portfolio Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  import axios from 'axios';
+  export default  {
+    name: 'portfolio',
+    props: [],
+    async mounted(){
+    {this.datos = await axios.get(this.url)}
+    },
+    data () {
+      return {
+        url:'/json/info.json',
+        datos:null
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+/*https://zyro.com/preview/eren */
+</style>
